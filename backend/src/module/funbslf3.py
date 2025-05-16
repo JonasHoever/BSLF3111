5ef1bc1395e7ce609caaadda742f591673899a1f
JonasHoever <jonas.hoever@edeka.de>
2025-05-16 09:45:10 +0200

    aufgabe 2 no comments

--- backend/src/module/funbslf3.py
+++ backend/src/module/funbslf3.py
@@ -34,7 +34,116 @@ class Create():                                                                 
             "subnet_addresses": subnetzadressen                                                     # Liste der Netzwerkadressen der Subnetze
         }
 
-    def symsubprefix(self, ipv4, countnet):
-        arr_basis_netz = list(map(int, ipv4.split(".")))
-        prefix = bin(arr_basis_netz[0])
-        return(prefix, arr_basis_netz[0])
+    def symsubprefix(self, ipv4, prefix, countnet):
+        """
+        Berechnet 'countnet' aufeinanderfolgende Subnetze
+        mit einer festen 'prefix'-Länge, beginnend bei 'ipv4'.
+        """
+        # Validierung der Eingaben
+        if not (isinstance(prefix, int) and 0 <= prefix <= 32):
+            raise ValueError("Prefix muss eine ganze Zahl zwischen 0 und 32 sein.")
+        if countnet <= 0:
+            raise ValueError("Anzahl der Subnetze muss größer als 0 sein.")
+
+        # --- Variablen-Initialisierung nach außen verschoben ---
+        netzmaske_bits = prefix
+        netzmaske_dez = [] # Hier initialisiert
+        temp_prefix_length = netzmaske_bits
+
+        # Berechnung der Netzmaske
+        for i in range(4):
+            if temp_prefix_length >= 8:
+                netzmaske_dez.append(255)
+                temp_prefix_length -= 8
+            else:
+                netzmaske_dez.append(256 - (2 ** (8 - temp_prefix_length)))
+                temp_prefix_length = 0
+        netzmaske_str = ".".join(map(str, netzmaske_dez)) # Hier zugewiesen
+
+        # Weitere Variablen initialisiert
+        host_bits = 32 - netzmaske_bits
+        usable_hosts = (2 ** host_bits) - 2 # -2 für Netz- und Broadcastadresse
+        subnet_block_size = 2 ** host_bits
+        results = []
+        
+        # Start-IP in Integer umwandeln
+        start_ip_int = 0
+        arr_ipv4_start = list(map(int, ipv4.split(".")))
+        for i, octet in enumerate(arr_ipv4_start):
+            start_ip_int += octet << (8 * (3 - i))
+        # --- Ende der Variablen-Initialisierung ---
+            
+        # Schleife für jedes der 'countnet' Subnetze
+        for i in range(countnet):
+            current_base_ip_int = start_ip_int + (i * subnet_block_size)
+            
+            # Überprüfen auf Überlauf über die IPv4-Grenze hinaus
+            if current_base_ip_int > 0xFFFFFFFF:
+                # Hier ist es besser, die Schleife zu beenden, anstatt einen ValueError zu werfen,
+                # da es sich um eine natürliche Grenze der Berechnung handelt und nicht unbedingt
+                # um einen "Fehler" in den Eingabeparametern.
+                break 
+            
+            # Aktuelle Integer-IP zurück in dotted-decimal Format umwandeln
+            current_base_ip_octets = []
+            for j in range(4):
+                current_base_ip_octets.append((current_base_ip_int >> (8 * (3 - j))) & 0xFF)
+            current_base_ipv4_str = ".".join(map(str, current_base_ip_octets))
+
+            # Netzwerkadresse berechnen
+            network_address_arr = []
+            for k in range(4):
+                # Hier war der Fehler: netzmaske_dez muss Elemente enthalten.
+                network_address_arr.append(current_base_ip_octets[k] & netzmaske_dez[k])
+            network_address = ".".join(map(str, network_address_arr))
+            
+            # Broadcast-Adresse berechnen
+            broadcast_address_arr = []
+            for k in range(4):
+                # Verwende bitweises OR mit invertierter Netzmaske (255 ^ netzmaske_dez[k]) für korrekte Broadcast
+                broadcast_address_arr.append(current_base_ip_octets[k] | (255 ^ netzmaske_dez[k]))
+            broadcast_address = ".".join(map(str, broadcast_address_arr))
+
+            # Erster und letzter Host
+            first_host = "N/A"
+            last_host = "N/A"
+            if usable_hosts > 0:
+                first_host_arr = list(network_address_arr)
+                first_host_arr[3] += 1
+
+                last_host_arr = list(broadcast_address_arr)
+                last_host_arr[3] -= 1
+
+                # Behandle Überträge/Unterläufe
+                for octet_idx in range(3, -1, -1): # Schleife von 3 bis 0, auch das erste Oktett prüfen
+                    # Erster Host
+                    if first_host_arr[octet_idx] > 255:
+                        first_host_arr[octet_idx] = 0
+                        if octet_idx > 0: first_host_arr[octet_idx - 1] += 1
+                    elif first_host_arr[octet_idx] < 0:
+                        first_host_arr[octet_idx] = 255
+                        if octet_idx > 0: first_host_arr[octet_idx - 1] -= 1
+                    
+                    # Letzter Host
+                    if last_host_arr[octet_idx] < 0:
+                        last_host_arr[octet_idx] = 255
+                        if octet_idx > 0: last_host_arr[octet_idx - 1] -= 1
+                    elif last_host_arr[octet_idx] > 255:
+                        last_host_arr[octet_idx] = 0
+                        if octet_idx > 0: last_host_arr[octet_idx - 1] += 1
+                    
+                first_host = ".".join(map(str, first_host_arr))
+                last_host = ".".join(map(str, last_host_arr))
+            
+            results.append({
+                "subnet_number": i + 1,
+                "base_ipv4_input_for_this_subnet": current_base_ipv4_str,
+                "prefix_length": f"/{prefix}",
+                "subnet_mask": netzmaske_str,
+                "network_address": network_address,
+                "broadcast_address": broadcast_address,
+                "usable_hosts": usable_hosts,
+                "first_host": first_host,
+                "last_host": last_host
+            })
+        return {"subnets": results}
